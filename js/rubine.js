9d7156b3f6d15df4c0b9057d6e16509223a649bf
Surya Akella <supra7@tamu.edu>
2016-11-24 19:23:43 -0600

    circle auto-correct

--- js/rubine.js
+++ js/rubine.js
@@ -13,3 +13,33 @@
 		}
 		return f8;
 	}
+
+// Need to check accuracy
+	function feature_f9(p){
+		var length = p.length
+		var f9 = 0;
+		var deltaxp = 0;
+		var deltayp = 0;
+		var deltaxp1 = 0;
+		var deltayp1 = 0;
+		var var1 = 0;
+		var var2 = 0;
+		for (var i = 1; i < length - 2; i++) {
+			deltaxp = p[i].getX() - p[i+1].getX();
+			deltayp = p[i].getY() - p[i+1].getY();
+			deltaxp1 = p[i-1].getX() - p[i].getX();
+			deltayp1 = p[i-1].getY() - p[i].getY();
+			var1 = deltaxp * deltayp1 - deltaxp1 * deltayp;
+			var2 = deltaxp * deltaxp1 + deltayp * deltayp1;
+			if(var2 == 0){
+				if (var1>0)
+					f9 += Math.atan(Infinity);
+				else
+					f9 += Math.atan(-Infinity);
+			}
+			else{
+				f9 += Math.atan(var1 / var2);
+			}
+		}
+		return f9;
+	}	
